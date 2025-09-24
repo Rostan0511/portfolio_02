fc1016a857a1556a945f20372f6d271b27c8158d
Rostan0511 <loborostan05@gmail.com>
2025-09-24 14:40:42 +0530

    destop on progress

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
 hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
   navLinks.classList.toggle('active');
 });
